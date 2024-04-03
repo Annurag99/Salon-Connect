849478422024eaf522173b4883bb61f15569345b
EC2 Default User <ec2-user@ip-172-31-41-248.eu-west-1.compute.internal>
2024-04-03 19:04:01 +0000

    changes1

--- salon_connect/asgi.py
+++ salon_connect/asgi.py
@@ -3,6 +3,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Salon_Connect.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'salon_connect.settings')
 
 application = get_asgi_application()
