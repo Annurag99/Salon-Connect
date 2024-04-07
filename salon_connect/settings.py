74945f2de6380a9e259397350b850a474d7dd40c
EC2 Default User <ec2-user@ip-172-31-31-232.eu-west-1.compute.internal>
2024-04-07 15:10:29 +0000

    Fixed bugs and security vulnerabilities

--- salon_connect/settings.py
+++ salon_connect/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# Load environment variables from .env file
 SECRET_KEY = config("SECRET_KEY")
+SONAR_LOGIN_TOKEN = config('SONAR_LOGIN_TOKEN')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,5 +126,3 @@ else:
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/static/'
