80fc6c3b8db955e13e77b9f73e4bac34b818b01e
EC2 Default User <ec2-user@ip-172-31-31-232.eu-west-1.compute.internal>
2024-04-07 20:43:29 +0000

    Added required key

--- salon_connect/settings.py
+++ salon_connect/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# Load environment variables from .env file
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 SONAR_LOGIN_TOKEN = config('SONAR_LOGIN_TOKEN')
 
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT  = BASE_DIR / "static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
