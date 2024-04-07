aaf53d55db774fb373c53ac956802ce44d591bfa
EC2 Default User <ec2-user@ip-172-31-31-232.eu-west-1.compute.internal>
2024-04-07 15:57:33 +0000

    Fixed bugs in html files

--- salon_connect/settings.py
+++ salon_connect/settings.py
@@ -118,10 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / "static"]
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
 else:
-    STATIC_ROOT  = BASE_DIR / "static"
+    STATICFILES_DIRS = [BASE_DIR / "static"]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
